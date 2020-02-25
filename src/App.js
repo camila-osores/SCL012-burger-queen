83ebbede03e534c8abdd5f382b601eb8f4cb51e9
JocelynJofreAntoine <joce.jofreantoine@gmail.com>
2020-02-25 13:29:44 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hola mundo!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <button id="btn_mesero" class="btn_mesero">MESERO</button>
+        <button id="btn_jefe" class="btn_jefe">JEFE DE COCINA</button>
+        
       </header>
     </div>
   );
