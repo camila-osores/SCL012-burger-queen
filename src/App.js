edd719bc10d35f14a3474081689657fb3d396b7d
camila-osores <camilaosoresa@gmail.com>
2020-03-06 23:40:37 -0300

    Agregando favicon, tipografía a botones en página de inicio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { render } from '@testing-library/react';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="btns_initial">
+            <button id="btn_waiter" className="btn_initial">MESERO</button>
+            <button id="btn_kitchen" className="btn_initial">COCINA</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 
-
-export default App;
+  export default App;
