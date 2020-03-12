dd6b5ba92cb4a761fdf910eaa9c63580de4c57c4
camila-osores <camilaosoresa@gmail.com>
2020-03-12 18:15:30 -0300

    estilos finales a botones de componente breakfast y tipografia

--- src/components/Breakfast.js
+++ src/components/Breakfast.js
@@ -19,29 +19,29 @@ const Breakfast = () => {
         <div className="container">
             <div className="row">
                 <div className="col-sm-12 col-md-6 col-lg-4">
-                    <h1 className=""><p className="display-4 mt-4" >Desayuno</p></h1>
+                    <h1 className=""><p className="display-4 mt-4 titles" >Desayuno</p></h1>
                         
                         <table class="table">
                             <thead class="thead-light">
                             
                                 <tr>
                                     <th scope="col"> Café americano $500 </th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Café americano' value="500">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Café americano' value="500">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Café con leche $700</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Café con leche' value="700"> AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Café con leche' value="700"> AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Sandwich de jamón y queso $1.000</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Sandwich de jamón y queso' value="1000"> AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Sandwich de jamón y queso' value="1000"> AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Jugo de frutas natural $750</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Jugo de frutas natural' value="750"> AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Jugo de frutas natural' value="750"> AGREGAR</button></th>
                                 </tr>
                         </thead>
                     </table>
@@ -50,7 +50,7 @@ const Breakfast = () => {
                 
 
                 <div className="col-sm-12 col-md-6 col-lg-4">
-                    <h1 className=""><p className="display-4 mt-4">Almuerzo</p></h1>
+                    <h1 className=""><p className="display-4 mt-4 titles">Almuerzo</p></h1>
                         
                         <table class="table">
                             <thead class="thead-light">
@@ -61,54 +61,54 @@ const Breakfast = () => {
 
                                 <tr>
                                     <th scope="col">Hamburguesa simple res $1.000</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Hamburguesa simple res' value="$1.000">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Hamburguesa simple res' value="$1.000">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Hamburguesa simple pollo $1.000</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Hamburguesa simple pollo' value="$1.000">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Hamburguesa simple pollo' value="$1.000">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Hamburguesa simple veg $1.000</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Hamburguesa simple vegetariana' value="$1.000">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Hamburguesa simple vegetariana' value="$1.000">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Hamburguesa doble res $1.500</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Hamburguesa doble res' value="$1.500">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Hamburguesa doble res' value="$1.500">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Hamburguesa doble pollo $1.500</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Hamburguesa doble pollo' value="$1.500">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Hamburguesa doble pollo' value="$1.500">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Hamburguesa doble veg $1.500</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Hamburguesa doble vegetariana' value="$1.500">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Hamburguesa doble vegetariana' value="$1.500">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Con agregado de huevo $100</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Agregado de huevo' value="$100">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Agregado de huevo' value="$100">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Con agregado de queso $100</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Agregado de queso' value="$100">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Agregado de queso' value="$100">AGREGAR</button></th>
                                 </tr>
 
                                 <th scope="col"><h5>Acompañamiento</h5></th><th scope="col"></th>
 
                                 <tr>
                                     <th scope="col">Papas fritas $500</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Papas fritas' value="$500">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Papas fritas' value="$500">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Aros de cebolla $500</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Aros de cebolla' value="500">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Aros de cebolla' value="500">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
@@ -117,22 +117,22 @@ const Breakfast = () => {
 
                                 <tr>
                                     <th scope="col">Agua 500 ml $500</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Agua 500 ml' value="$500">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Agua 500 ml' value="$500">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Agua 750 ml $700</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Agua 750 ml' value="$700">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Agua 750 ml' value="$700">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Bebida 500 ml $700</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Bebida 500 ml' value="$700">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Bebida 500 ml' value="$700">AGREGAR</button></th>
                                 </tr>
 
                                 <tr>
                                     <th scope="col">Bebida 750 ml $1.000</th>
-                                    <th scope="col"><button onClick={agregar} className="btn btn-success" name='Bebida 750 ml' value="$1.000">AGREGAR</button></th>
+                                    <th scope="col"><button onClick={agregar} className="btn agregar" name='Bebida 750 ml' value="$1.000">AGREGAR</button></th>
                                 </tr>
 
                             </thead>
@@ -141,7 +141,7 @@ const Breakfast = () => {
                 </div>
 
                 <div className="col-sm-12 col-md-6 col-lg-4 orden">
-                <h1 className=""><p className="display-4 mt-4">Orden</p></h1>
+                <h1 className=""><p className="display-4 mt-4 titles">Orden</p></h1>
 
                    {cuenta.map(item=>(
                        <li key={item.id}>{item.nombre} : {item.valor}</li>
